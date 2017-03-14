e8be08348a59831802fb93e1a42be096f7e8901b
YIFAN YANG <yifanyang@smtp.boletos.com.br>
2017-03-13 22:59:47 -0500

    1.02 Add ExtensionMethod "FaceToCamera", extened Tranform class to re-oriented object on the HoloCube to face camera. Add Input example with scripts, demo how to do gaze + button click input in HoloCube, the input system can also handle phone/tablet screen tap input.

---
+++ Assets/Examples/Inputs/Scripts/InputExampleManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputExampleManager : MonoBehaviour {
+	public Transform cubeRoot;
+	public Renderer [] cubeSides;
+	int cubeClickCount = 1;
+
+	public Color [] colorPool;
+	int colorIndex = 0;
+
+	public bool alwaysReMatch = false;
+	bool alreadyMatch = false;
+	void Start(){
+		GetComponent<BasicTrackableEventHandler>().OnTrackingFound += OnTrackingFound;
+		GetComponent<BasicTrackableEventHandler>().OnTrackingLost += OnTrackingLost;
+	}
+	void OnTrackingFound(){
+		if (!alreadyMatch) {
+			transform.FaceToCamera (cubeRoot);
+			alreadyMatch = !alwaysReMatch;
+		}
+	}
+	void OnTrackingLost(){
+		
+	}
+
+	public void CubeClick(){
+		cubeClickCount++;
+		if (cubeClickCount > 5) {
+			cubeClickCount = 1;
+		}
+		Vector2 sizeTp = Vector2.one * (float)cubeClickCount;
+		foreach (Renderer tp in cubeSides) {
+			tp.material.SetTextureScale ("_decal", sizeTp);
+		}
+	}
+
+	public void NextClick(){
+		colorIndex++;
+		if (colorIndex >= colorPool.Length) {
+			colorIndex = 0;
+		}
+		foreach (Renderer tp in cubeSides) {
+			tp.material.SetColor("_basecolor", colorPool[colorIndex]);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # HoloCube-by-Merge-Examples
 Examples of what you can develop with HoloCube by Merge.
+
+Release Notes for version 1.02
+===========
+Add ExtensionMethod "FaceToCamera", extened Tranform class to re-oriented object on the HoloCube to face camera.
+Add Input example with scripts, demo how to do gaze + button click input in HoloCube, the input system can also handle phone/tablet screen tap input.
+
+Release Notes for version 1.01
+===========
+Fix bug one side of cube is flipped.
+
 Release Notes for version 1.0
 ===========
 Include Inside Cube, BoomBox and Firework.
@@ -10,7 +20,5 @@ BoomBox: Demo how to replace HoloCube by Merge with other object and handle audi
 
 Firework:Demo how to shoot stuff from the cube and view it in real world space.  
 
-Release Notes for version 1.01
-===========
-Fix bug one side of cube is flipped.
+
 
